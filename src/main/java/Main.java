c8019996ff4a9cc68bb187c8a2842184da78cd70
federico <federico@menxit.com>
2018-01-01 16:17:10 +0100

    maven

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,22 +8,25 @@ public class Main {
     public static void main(String[] args) {
 
         FactoryTree factoryTree = new FactoryTree();
-        SpeedupBinaryTreeAdder speedupBinaryTreeAdder = new SpeedupBinaryTreeAdder();
+        SpeedupBinaryTreeAdder speedupBinaryTreeAdder;
 
         Seriale warmup = new Seriale();
         warmup.computeOnerousSum(factoryTree.randomBinaryTree(5));
 
         for(int i = 8; i <= 20; i = i+4) {
             Node tree = factoryTree.randomBinaryTree(i);
+            speedupBinaryTreeAdder = new SpeedupBinaryTreeAdder();
             Double speedup;
 
-            speedup = speedupBinaryTreeAdder.getSpeedUp(new Seriale(), new BufferBinaryTreeAdder(), tree);
+            Seriale seriale = new Seriale();
+
+            speedup = speedupBinaryTreeAdder.getSpeedUp(seriale, new BufferBinaryTreeAdder(), tree);
             System.out.println("HWJ1, height = " + i + ":\t" + speedup);
 
-            speedup = speedupBinaryTreeAdder.getSpeedUp(new Seriale(), new BufferBinaryTreeAdder(tree.getMaxDepth()), tree);
+            speedup = speedupBinaryTreeAdder.getSpeedUp(seriale, new BufferBinaryTreeAdder(tree.getMaxDepth()), tree);
             System.out.println("HWJ2, height = " + i + ":\t" + speedup);
 
-            speedup = speedupBinaryTreeAdder.getSpeedUp(new Seriale(), new ForkJoinBinaryTreeAdder(), tree);
+            speedup = speedupBinaryTreeAdder.getSpeedUp(seriale, new ForkJoinBinaryTreeAdder(), tree);
             System.out.println("HWJ3, height = " + i + ":\t" + speedup);
 
             System.out.println("======================================\n");
--- src/main/java/Speedup/SpeedupBinaryTreeAdder.java
+++ src/main/java/Speedup/SpeedupBinaryTreeAdder.java
@@ -3,11 +3,17 @@ package Speedup;
 import HWJ.BinaryTreeAdder;
 import Tree.Node;
 
+import java.util.HashMap;
+
 public class SpeedupBinaryTreeAdder {
+
+    private Double benchmarkType1 = null;
 
     public double getSpeedUp(BinaryTreeAdder type1, BinaryTreeAdder type2, Node node) {
         BenchmarkBinaryTreeAdder benchmarkBinaryTreeAdder = new BenchmarkBinaryTreeAdder();
-        double benchmarkType1 = benchmarkBinaryTreeAdder.getBenchmark(type1, node);
+        if(benchmarkType1 == null) {
+            benchmarkType1 = benchmarkBinaryTreeAdder.getBenchmark(type1, node);
+        }
         double benchmarkType2 = benchmarkBinaryTreeAdder.getBenchmark(type2, node);
         return Math.floor((benchmarkType1/benchmarkType2)*10)/10;
     }
